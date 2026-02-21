2440edb8a1d345aa0e7b191243637581fade5f41
Davy917 <davyhimself917@gmail.com>
2026-02-21 16:11:57 +0800

    fix

--- 707-my-linked-list/P0707_MyLinkedList.java
+++ 707-my-linked-list/P0707_MyLinkedList.java
@@ -29,24 +29,6 @@ public class P0707_MyLinkedList{
     }
 
     public void addAtHead(int val){
-        /*
-                在 deleteAtHead 方法中，你是要刪除鏈表的第一個節點（head）。
-
-        看這個例子：
-        head--->[A | -]---> [B | -]---> [C | ]
-
-        執行 deleteAtHead() 後，你想要的結果是：
-        head---> [B | -]---> [C | ]
-
-        所以邏輯是：
-            head 目前指向節點 A
-            head.next 指向節點 B
-            this.head = head.next; 讓 head 改為指向節點 B
-
-        這樣就刪除了節點 A。
-        如果寫成 this.head = head.next.next;
-        那就會跳過節點 B 直接指向節點 C，這樣反而刪除了節點 A 和節點 B，不符合需求。
-         */
         ListNode newNode = new ListNode(val);
         newNode.next = this.head;
         this.head = newNode;
