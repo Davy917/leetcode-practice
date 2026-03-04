97398faa7b8df1fc4352637c215e02a2ba88e54d
Davy917 <davyhimself917@gmail.com>
2026-03-04 16:29:07 +0800

    優化 visit 方法中的節點操作，增強瀏覽歷史管理邏輯

--- 1472-browse-history/BrowserHistory.java
+++ 1472-browse-history/BrowserHistory.java
@@ -58,9 +58,9 @@ class BrowserHistory {
         //从当前页跳转访问 url 对应的页面 。执行此操作会把浏览历史前进的记录全部删除。
         System.out.printf("你原本在瀏覽%s", current.nodeName + "。");
         Node newNode = new Node(url, ++seq);//注意++seq 與 seq++不同
-        newNode.prev = current;
-        current.next = newNode;
-        current = newNode;//注意
+        newNode.prev = current;//讓新節點知道「上一頁是誰」
+        current.next = newNode;//讓舊的 current 知道「下一頁是誰」
+        current = newNode;//告訴 BrowserHistory：「我現在人在 newNode 這頁了」（很關鍵）
         System.out.printf("訪問%s%n", url);
         channelList.printList(channelList.head);
     }
@@ -115,15 +115,4 @@ class BrowserHistory {
  * obj.visit(url);
  * String param_2 = obj.back(steps);
  * String param_3 = obj.forward(steps);
- */
-
-/*
-你有一个只支持单个标签页的 浏览器，最开始你浏览的网页是homepage，你可以访问其他的网站url也可以在浏览历史中后退steps步或前进steps步。
-
-请你实现 BrowserHistory 类：
-
-BrowserHistory(string homepage) ，用 homepage 初始化浏览器类。
-void visit(string url) 从当前页跳转访问 url 对应的页面  。执行此操作会把浏览历史前进的记录全部删除。
-string back(int steps) 在浏览历史中后退 steps 步。如果你只能在浏览历史中后退至多 x 步且 steps > x ，那么你只后退 x 步。请返回后退 至多 steps 步以后的 url 。
-string forward(int steps) 在浏览历史中前进 steps 步。如果你只能在浏览历史中前进至多 x 步且 steps > x ，那么你只前进 x 步。请返回前进 至多 steps步以后的 url 。
  */
