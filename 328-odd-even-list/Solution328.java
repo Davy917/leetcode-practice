353441ac9603c532115e46b3d9f9a7cc39aa40e8
Davy917 <davyhimself917@gmail.com>
2026-03-18 05:58:08 +0800

    進度更新

--- 328-odd-even-list/Solution328.java
+++ 328-odd-even-list/Solution328.java
@@ -78,14 +78,6 @@ class Solution328 {
         return head;
     }
 /*
-temp = fast.next;
-fast.next = fast.next.next;
-temp.next = slow.next;
-slow.next = temp;
-//移動指針
-slow = slow.next;
-fast = fast.next.next;
-
 slow     fast     temp
   ↓        ↓        ↓
 [2|-]--->[1|-]--->[3|-]--->[5|-]--->[6|-]--->[4|-]--->[7|-]--->null
@@ -95,16 +87,51 @@ slow     fast     temp
                    slow                      fast     temp
                     ↓                          ↓        ↓
 [2|-]--->[3|-]--->[6|-]--->[1|-]--->[5|-]--->[4|-]--->[7|-]--->null
- */
 
+                   slow                      fast
+                    ↓                          ↓
+[2|-]--->[3|-]--->[6|-]--->[7|-]--->[1|-]--->[5|-]--->[4|-]--->null
+
+                           slow                      fast
+                            ↓                          ↓
+[2|-]--->[3|-]--->[6|-]--->[7|-]--->[1|-]--->[5|-]--->[4|-]--->null
+ */
+public ListNode oddEvenList_v2(ListNode head) {
+    if (head == null || head.next == null){
+        return head;
+    }
+
+    ListNode slow = head;
+    ListNode fast = head.next;
+    ListNode temp;
+
+    while (fast.next != null){
+        temp = fast.next;
+        fast.next = fast.next.next;
+        temp.next = slow.next;
+        slow.next = temp;
+
+        //移動指針
+//        if (fast.next != null){
+//            fast = fast.next;
+//        }
+        fast = fast.next;
+        slow = slow.next;
+        mainList.printList(head);
+        System.out.println("slow.val" + slow.val);
+        System.out.println("fast.val" + fast.val);
+    }
+    return head;
+}
     static void main(String[] args) {
-        int[] nums = {3, 2, 1};
+        int[] nums = {2, 1, 3, 5, 6, 4, 7};
         Solution328 sol = new Solution328();
         for (int i : nums){
             ListNode newNode = new ListNode(i, null, null);
             sol.mainList.AddAtTail(newNode);
         }
-        sol.oddEvenList(sol.mainList.head);
+//        sol.oddEvenList(sol.mainList.head);
+        sol.oddEvenList_v2(sol.mainList.head);
     }
 }
 /*
