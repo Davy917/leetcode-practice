2418deeacb752e0e4f6379e7219bfc618215e354
Davy917 <davyhimself917@gmail.com>
2026-03-21 20:40:59 +0800

    新增快慢指針解法

---
+++ 876-middle-node/solution.py
@@ -0,0 +1,47 @@
+#快慢指針
+from typing import Optional
+class ListNode:
+    def __init__(self, val, next) -> None:
+        self.val = val
+        self.next = next
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+    def AddAtTail(self, newNode):
+        if self.head is None:
+            self.head = newNode
+            return
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = newNode
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            print("fast = ", fast.val)
+            print("slow", slow.val)
+
+        if fast.next is None:
+            print("ans = ", slow.val)
+            return slow
+        else:
+            slow = slow.next
+            print("ans = ", slow.val)
+            return slow
+
+if __name__ == "__main__":
+    nums = [1,2]
+    sol = Solution()
+    mainList = LinkedList()
+    for i in nums:
+        newNode = ListNode(i, None)
+        mainList.AddAtTail(newNode)
+    sol.middleNode(mainList.head)
+        
