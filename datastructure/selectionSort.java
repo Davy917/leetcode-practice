49fcaf39831ce22edef719a61e260f0063044bf1
Davy917 <davyhimself917@gmail.com>
2026-03-22 20:43:48 +0800

    新增選擇排序解法

---
+++ datastructure/selectionSort.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class selectionSort{
+    int[] selectionSort(int[] arr) {
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.printf("As is %s%n", Arrays.toString(arr));
+            minIndex = i;
+            System.out.printf("min = %s%n", arr[minIndex]);
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.printf("min %s compare with", arr[minIndex]);
+                System.out.printf("cur %s%n", arr[j]);
+                if (arr[minIndex] > arr[j]) {
+                    // 记录最小值的下标
+                    System.out.println("find smaller val " + arr[j]);
+                    minIndex = j;
+                }
+            }
+            // 将最小元素交换至首位
+            int temp = arr[i];
+            arr[i] = arr[minIndex];//選擇排序法是不穩定排序法, 原因就是它是把值, 直接插入首端或尾端
+            arr[minIndex] = temp;
+            System.out.printf("To be %s%n", Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        selectionSort t = new selectionSort();
+        int[] ans = t.selectionSort(new int[]{5, 8, 6, 3, 9, 1});
+        System.out.println(Arrays.toString(ans));
+    }
+}
