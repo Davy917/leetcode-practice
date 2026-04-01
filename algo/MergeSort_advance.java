c714725f675756c8677d00e9b9635e1b785c4141
Davy917 <davyhimself917@gmail.com>
2026-04-01 20:05:32 +0800

    完善MergeSort_advance.java和MergeSort_basic.java中的註解，清理不必要的代碼

--- algo/MergeSort_advance.java
+++ algo/MergeSort_advance.java
@@ -6,6 +6,13 @@ public class MergeSort_advance {
         int[] result = new int[arr.length];
         mergeSort(arr, 0, arr.length - 1, result);
     }
+    /**
+     *
+     * @param arr 原始陣列
+     * @param start 原始陣列的起始位置下標
+     * @param end 原始陣列的結束位置下標
+     * @param result 額外空間,用來裝合併後的數組
+     */
     private static void mergeSort(int[] arr, int start, int end, int[] result){
         if (start == end) return;
         int middle = (start + end) / 2;
@@ -16,7 +23,7 @@ public class MergeSort_advance {
         System.out.println("拆分右邊");
         mergeSort(arr, middle + 1, end, result);
 
-        //決定要用一般版本還是simplift版本
+        //決定要用哪一個版本
         //merge_simplify(arr, start, end, result);
         merge(arr, start, end, result);
     }
@@ -24,6 +31,7 @@ public class MergeSort_advance {
     在这份代码中，我们统一使用 result 数组作为递归过程中的临时数组，所以merge 函数接收的参数不再是两个数组，而是 result 数组中需要合并的两个数组的首尾下标。
     根据首尾下标可以分别计算出两个有序数组的首尾下标 start1、end1、start2、end2，之后的过程就和之前合并两个有序数组的代码类似了。
      */
+
     public static void merge(int[] arr, int start, int end, int[] result){
         int middle = (start + end)/2;
         //數組1的首尾位置
@@ -66,7 +74,6 @@ public class MergeSort_advance {
         /*
         我们可以去掉一些不会改变的临时变量。
         比如 start1 始终等于 start，end2 始终等于 end，end1 始终等于 middle。
-        并且分析可知，resultIndex 的值始终等于 start 加上 index1 和 index2 移动的距离。
          */
         int middle = (start + end)/2;
         int start2 = middle + 1;
@@ -102,3 +109,12 @@ public class MergeSort_advance {
         System.out.println(Arrays.toString(arr));
     }
 }
+/*
+第76行為何不是result[index1 + index2] = arr[index1];
+原因是：
+初始時 index1 = start、index2 = start2
+    你希望第一次寫入的 result index 是 start
+    所以用：index1 + index2 - start2
+    代入初始值：start + start2 - start2 = start（剛好從 start 開始）
+    如果用 index1 + index2，第一次就會是 start + start2，會超過 end 很多，直接越界或至少位置完全不對。
+ */
--- algo/MergeSort_basic.java
+++ algo/MergeSort_basic.java
@@ -1,19 +1,46 @@
 import java.util.Arrays;
-/*
-只要开辟一个长度等同于两个数组长度之和的新数组，
-并使用两个指针来遍历原有的两个数组，
-不断将较小的数字添加到新数组中，
-并移动对应的指针即可。
 
-首先开辟了一个新数组 result，长度等同于 arr1 和 arr2 的长度之和
-，然后使用 index1 记录 arr1 数组的下标，index2 记录 arr2 数组的下标。
-再将两个数组中较小的值不断添加到 result 中。其中，result 的当前下标等同于 index1 和 index2 之和。
-//result 的当前下标等同于 index1 和 index2 之和
- */
 public class MergeSort_basic {
+    public static void mergeSort(int[] arr){
+        if (arr.length == 0)
+            return;
+        int[] result = mergeSort(arr, 0, arr.length - 1);
+        System.arraycopy(result, 0, arr, 0, result.length);
+    }
+
+    private static int[] mergeSort(int[] arr, int start, int end){
+        if (start == end) {
+            /*
+            代表建立一個長度為 1 的陣列，裡面唯一的元素是 arr[start]。
+            例如如果 arr[start] 是 6，實際回傳的是 int[]，內容為 [6]。
+            在你的遞迴中，這是「子問題只剩一個元素」時的基底回傳值。
+             */
+            return new int[]{arr[start]};
+        }
+        int middle = (start + end)/2;
+        //拆分左邊
+        int[] left = mergeSort(arr, start, middle);//不是middle - 1?
+        //拆分右邊
+        int[] right = mergeSort(arr, middle + 1, end);
+        //合併兩邊
+        return merge(left, right);
+    }
+
+    /*
+    只要开辟一个长度等同于两个数组长度之和的新数组，
+    并使用两个指针来遍历原有的两个数组，
+    不断将较小的数字添加到新数组中，
+    并移动对应的指针即可。
+
+    首先开辟了一个新数组 result，长度等同于 arr1 和 arr2 的长度之和
+    ，然后使用 index1 记录 arr1 数组的下标，index2 记录 arr2 数组的下标。
+    再将两个数组中较小的值不断添加到 result 中。其中，result 的当前下标等同于 index1 和 index2 之和。
+     */
+
     static int[] merge(int[] arr1, int[] arr2){
         //在 Java 中，要建立固定長度的陣列物件，本質上都需要配置新物件，所以離不開 new（直接或隱含）。
         int[] result = new int[arr1.length + arr2.length];
+
         int index1 = 0, index2 = 0;
         while (index1 < arr1.length && index2 < arr2.length){
             if (arr1[index1] <= arr2[index2]){
@@ -37,37 +64,6 @@ public class MergeSort_basic {
         }
         System.out.println(Arrays.toString(result));
         return result;
-    }
-
-    public static void mergeSort(int[] arr){
-        //先寫一個預防邊界case
-        if (arr.length == 0)
-            return;
-
-        int[] result = mergeSort(arr, 0, arr.length - 1);
-
-//        for (int i = 0; i < result.length; i++)
-//            arr[i] = result[i];
-        System.arraycopy(result, 0, arr, 0, result.length);
-    }
-    //2, 3, 5, 4, 6, 1 To understand
-    private static int[] mergeSort(int[] arr, int start, int end){
-        if (start == end) {
-            /*
-            代表建立一個長度為 1 的陣列，裡面唯一的元素是 arr[start]。
-            例如如果 arr[start] 是 6，實際回傳的是 int[]，內容為 [6]。
-            在你的遞迴中，這是「子問題只剩一個元素」時的基底回傳值。
-             */
-            System.out.println("arr[start] = " + arr[start]);
-            return new int[]{arr[start]};
-        }
-        int middle = (start + end)/2;
-        //拆分左邊
-        int[] left = mergeSort(arr, start, middle);//不是middle - 1?
-        //拆分右邊
-        int[] right = mergeSort(arr, middle + 1, end);
-        //合併兩邊
-        return merge(left, right);
     }
 
     static void main(String[] args) {
