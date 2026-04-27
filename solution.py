35161aeb45580622089fb07dbc0643297acf8c30
Davy917 <davyhimself917@gmail.com>
2026-04-27 15:50:46 +0800

    Implement isPerfectSquare method in Solution class

---
+++ solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        left = 1
+        right = num
+        while left <= right:
+            middle = left + (right - left) // 2
+            if middle * middle > num:
+                right = middle - 1
+            elif middle * middle < num:
+                left = middle + 1
+            else:
+                return True
+        return False
+if __name__ =="__main__":
+    num = 2 ** 31 - 1
+    print("ans = ", Solution().isPerfectSquare(num))
