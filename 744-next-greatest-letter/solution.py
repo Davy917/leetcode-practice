fb8b754e55abac08e3f338ffa2bc21f880bd2fd9
Davy <davyhimself917@gmail.com>
2026-04-27 20:48:08 +0800

    進度提交

--- 744-next-greatest-letter/solution.py
+++ 744-next-greatest-letter/solution.py
@@ -1,23 +1,24 @@
+"""
+參考代碼模板:
+algo\BinarySearch\BinarySearch_basic
+"""
 class Solution(object):
     def nextGreatestLetter(self, letters, target):
         left = 0
         right = len(letters) - 1
+        
         while left < right:
             middle = left + (right - left) // 2
             print(f"left = {left}, right = {right}, middle = {middle}")
-            if letters[middle] < target:
+            if letters[middle] <= target:
                 left = middle + 1
-            elif letters[middle] > target:
+            else:
                 right = middle
 
-            else:
-                right = middle + 1
-                return letters[right]
         return letters[right] if letters[right] > target else letters[0]
 
-
 if __name__ == "__main__":
-    letters = ['c', 'f', 'j', 'l', 'b', 'v']
+    letters = ['c', 'f', 'j', 'l', 'v', 'w', 'y']
     letters2 = ['e','e','g','g']
     target = 'g'
     print("Ans = ", Solution().nextGreatestLetter(letters2, target))
