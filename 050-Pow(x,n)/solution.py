e2d1ac56850c9e842913cf3c72973f683ed53f0e
Davy917 <davyhimself917@gmail.com>
2026-04-27 13:31:05 +0800

    Refactor myPow for efficient exponentiation
    Refactor myPow method to implement exponentiation by squaring for improved efficiency.

--- 050-Pow(x,n)/solution.py
+++ 050-Pow(x,n)/solution.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    #修正前
     def myPow(self, x, n) -> float:
         result = 1
         half = abs(n) // 2
@@ -10,8 +11,26 @@ class Solution(object):
                 result *= x
               
         return result * result * x if n & 1 else result * result
-      
+    #修正後
+    def myPow(self, x, n) -> float:
+        result = 1
+        #負數n預處理
+        if n < 0:
+            x = 1 / x
+            n = abs(n)
+        while n > 0:
+            if n & 1:
+                result *= x
+            x *= x
+            n //= 2
+        return result
+        
 if __name__ == "__main__":
     x = 2.0
     n = -200000000
     print("result = ", Solution().myPow(x, n))
+
+"""
+快速冪影片教學
+https://youtu.be/GbDtCFhq20A?si=tWEqyB_81Zd6tiRe
+"""
