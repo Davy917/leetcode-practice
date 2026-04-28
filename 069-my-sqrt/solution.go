2db6a5bc075e47bb5d46a80f8fbe9e323c3a6280
Davy <davyhimself917@gmail.com>
2026-04-28 21:29:37 +0800

    修改檔名

--- 069-my-sqrt/solution.go
+++ 069-my-sqrt/solution.go
@@ -1,22 +1,24 @@
 package main
+
 /*
 代碼參考模板:
 algo/BinarySearch/BinarySearch_basic.java
 */
 import "fmt"
-func mySqrt(x int) int{
+
+func mySqrt(x int) int {
 	var left int = 1
 	var right int = x
 	var ans int = 0
 	for left <= right {
-		middle := left + (right - left) / 2
+		middle := left + (right-left)/2
 		fmt.Printf("left = %d, right = %d, middle666 = %d\n", left, right, middle)
-		if middle * middle > x {
+		if middle*middle > x {
 			right = middle - 1
-		}else if middle * middle < x {
+		} else if middle*middle < x {
 			left = middle + 1
 			ans = middle
-		}else {
+		} else {
 			ans = middle
 			return ans
 		}
@@ -26,5 +28,5 @@ func mySqrt(x int) int{
 func main() {
 	var x = 8
 	ans := mySqrt(x)
-	fmt.Println(ans)
+	fmt.Println("ans = ", ans)
 }
