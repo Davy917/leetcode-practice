ff385d58c7b0fe0fdc94bcd825f239c620e27892
Davy917 <davyhimself917@gmail.com>
2026-05-06 00:24:02 +0800

    Refactor RadixSort method to RadixSort_advance

--- algo/RadixSort/RadixSort_advance.java
+++ algo/RadixSort/RadixSort_advance.java
@@ -1,39 +1,42 @@
-package RadixSort;
-
 import java.util.Arrays;
 
 public class RadixSort_advance {
-
-    static void RadixSort(int[] arr){
-        int max_num = arr[0];
-        for (int val: arr){
-            if (Math.abs(val) > Math.abs(max_num)){
-                max_num = Math.abs(val);
-            }
+    static int[] RadixSort_advance(int[] arr){
+        int maxNum = Math.abs(arr[0]);
+        for (int value : arr)
+            maxNum = Math.max(Math.abs(maxNum), Math.abs(value));
+        int maxDigitLen = 0;
+        while (maxNum > 0){
+            maxNum /= 10;
+            maxDigitLen++;
         }
-        int maxDigitLength = 0;
-        while (max_num > 0){
-            max_num /= 10;
-            maxDigitLength += 1;
-        }
-        System.out.println(maxDigitLength);
-
         int dev = 1;
-        for (int i = 0; i < maxDigitLength; i++) {
+        int[] result = new int[arr.length];
+        while (dev < Math.pow(10, maxDigitLen)){
             int[] counting = new int[19];
-            for (int val: arr){
-                int radix = val / dev % 10 + 9;
-                counting[radix] += 1;
+            for (int value : arr)
+                counting[value / dev % 10 + 9]++;
+            System.out.println("counting before prefix" + Arrays.toString(counting));
+            int preSum = 0;
+            for (int index = 0; index < counting.length; index++){
+                int temp = counting[index];
+                counting[index] = preSum;
+                preSum += temp;
             }
-            System.out.println("counting = " + Arrays.toString(counting));
-            //TODO
-
+            System.out.println("counting after prefix" + Arrays.toString(counting));
+            for (int value : arr){
+                int radix = value / dev % 10 + 9;
+                result[counting[radix]] = value;
+                counting[radix]++;
+            }
+            arr = Arrays.copyOf(result, result.length);
             dev *= 10;
         }
+        return result;
     }
-
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = {520, -211, 438, -888, 7, 111, 985, 666, -996, 233, 168};
-        RadixSort(arr);
+        int[] arr2 = {520, 211, 438, 888, 7, 111, 985, 666, 996, 233, 168};
+        System.out.println("Ans = " + Arrays.toString(RadixSort_advance(arr)));
     }
 }
