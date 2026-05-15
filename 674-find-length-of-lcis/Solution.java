96b03b5a7b86cb226465b6385b013b9752fdb32f
Davy <davyhimself917@gmail.com>
2026-05-15 21:35:11 +0800

    進度提交

---
+++ 674-find-length-of-lcis/Solution.java
@@ -0,0 +1,11 @@
+public class Solution674 {
+    public static int findLengthOfLCIS(int[] nums) {
+        int maxLCIS = 0;
+        
+        return maxLCIS;
+    }
+    static void main(String[] args) {
+        int[] nums = {1, 3, 5, 4, 7};
+        System.out.println("Ans = ", findLengthOfLCIS(nums));
+    }
+}
