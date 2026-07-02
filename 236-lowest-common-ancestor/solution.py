d05804ca0dd8c4af079d569ed33127e987cec8d4
Davy <davyhimself917@gmail.com>
2026-07-03 05:55:42 +0800

    進度提交

---
+++ 236-lowest-common-ancestor/solution.py
@@ -0,0 +1,15 @@
+from datastructure import TreeNode
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+    def recursion(self, root: TreeNode, p: TreeNode, q: TreeNode) -> bool:
+        return
+
+
+"""
+k神解答:
+https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/solutions/240096/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+"""
