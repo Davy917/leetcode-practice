7978b428723e3f59e5bf113b3d36489c2e3f2c1a
Davy <davyhimself917@gmail.com>
2026-07-19 20:18:47 +0800

    進度提交

--- 1325-remove-leaf-nodes/Solution.java
+++ 1325-remove-leaf-nodes/Solution.java
@@ -1,21 +1,17 @@
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val, TreeNode left, TreeNode right){
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-class Solution {
-    public TreeNode removeLeafNodes(TreeNode root, int target) {
+import Tree.*;
+class Solution1325 {
+    public static TreeNode removeLeafNodes(TreeNode root, int target) {
         if (root == null)
             return null;
-        root.left = removeLeafNodes(root.left, target);
-        root.right = removeLeafNodes(root.right, target);
-        if (root.val == target && root.left == null && root.right == null)
+        root.setLeft(removeLeafNodes(root.getLeft(), target));
+        root.setRight(removeLeafNodes(root.getRight(), target));
+        if (root.getVal() == target && root.getLeft() == null && root.getRight() == null)
             return null;
         return root;
+    }
+    static void main(String[] args) {
+        Integer[] levelOrder = {1,2,3,2,null,2,4};
+        TreeNode root = TreeDebugger.buildLevelOrderTree(levelOrder);
+        System.out.println("Ans = " + removeLeafNodes(root, 2));
     }
 }
