ff1ca5991d5eb33cf4e023aa884bc243b4156df3
Davy <davyhimself917@gmail.com>
2026-08-15 21:06:27 +0800

    進度提交

---
+++ datastructure/QuickUnion/QuickFind.java
@@ -0,0 +1,27 @@
+package QuickUnion;
+
+public class QuickFind {
+    int[] root;
+
+    public QuickFind(int size){
+        root = new int[size];
+        for(int i = 0; i < size; i++)
+            root[i] = i;
+    }
+
+    int find(int x){
+        return root[x];
+    }
+    void union(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY)
+            for (int i = 0; i < root.length; i++)
+                if (root[i] == rootY)
+                    root[i] = rootX;
+    }
+
+    boolean connected(int x, int y){
+        return find(x) == find(y);
+    }
+}
