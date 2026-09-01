f45a0aa7780e383896fb638db54fb881a22a1479
Davy <davyhimself917@gmail.com>
2026-09-01 21:21:22 +0800

    進度提交

--- 695-max-area-of-island/Solution.java
+++ 695-max-area-of-island/Solution.java
@@ -2,23 +2,39 @@ import java.util.Arrays;
 
 class Solution695 {
     private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static int rows;
+    private static int cols;
     public static int maxAreaOfIsland(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
+        rows = grid.length;
+        cols = grid[0].length;
         int[][] color = new int [rows][cols];
 
         int maxArea = 0;
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++)
                 if (grid[i][j] == 1 && color[i][j] == 0){
-                    int curArea = dfs(grid, color, i, j);
-                    maxArea = Math.max(curArea, maxArea);
+                    int curArea = dfs(grid, color, i, j, 0);
+                    System.out.printf("i = %d, j = %d\n", i, j);
+                    if (curArea > maxArea){
+                        maxArea = curArea;
+                        System.out.println("find max area " + maxArea);
+                    }
                 }
-
         return maxArea;
     }
-    public static int dfs(int[][] grid, int[][] color, int i, int j){
-//TODO
+    public static int dfs(int[][] grid, int[][] color, int i, int j, int count){
+        count++;
+        color[i][j] = 1;
+        for(int[] direction: DIRECTIONS){
+            int newX = i + direction[0];
+            int newY = j + direction[1];
+            if (inGrid(newX, newY) && grid[newX][newY] == 1&& color[newX][newY] == 0)
+                dfs(grid, color, newX, newY, count);
+        }
+        return count;
+    }
+    public static boolean inGrid(int i, int j){
+        return i >= 0 && j >= 0 && i < rows && j < cols;
     }
     static void main(String[] args) {
         int[][] grid = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
@@ -32,3 +48,10 @@ class Solution695 {
         System.out.println("Ans = " + maxAreaOfIsland(grid));
     }
 }
+/*
+假設情境:
+1. 走到陸地, 走進dfs
+2. 將color[i][j]標為1
+3. 該陸地左右上下有未被遍歷過的土地, 就往該土地走
+TODO
+ */
